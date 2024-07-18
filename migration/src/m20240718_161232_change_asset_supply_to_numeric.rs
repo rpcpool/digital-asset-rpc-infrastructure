ff1ae72cf05e60855d70d11ed811a631204c8925
Kyle Espinola <kyle.s.espinola@gmail.com>
2024-07-18 17:09:26 +0200

    fix: switch supply to numeric type biging only supports i32

---
+++ migration/src/m20240718_161232_change_asset_supply_to_numeric.rs
@@ -0,0 +1,32 @@
+use crate::model::table::Asset;
+use sea_orm::{DatabaseBackend, Statement};
+use sea_orm_migration::{prelude::*, sea_orm::ConnectionTrait};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "ALTER TABLE asset ALTER COLUMN supply TYPE NUMERIC;".to_string(),
+            ))
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Asset::Table)
+                    .modify_column(ColumnDef::new(Asset::Supply).big_integer().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+}
