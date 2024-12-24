09d200218380e8bfd79dc903bd46084a5734f432
AhzamAkhtar <ahzam8186@gmail.com>
2024-12-24 21:03:39 +0530

    fungible tokens skipped when using showFungible option bug fixed in getAssetByOwner

--- digital_asset_types/src/dao/extensions/asset.rs
+++ digital_asset_types/src/dao/extensions/asset.rs
@@ -2,7 +2,7 @@ use sea_orm::{EntityTrait, EnumIter, Related, RelationDef, RelationTrait};
 
 use crate::dao::{
     asset, asset_authority, asset_creators, asset_data, asset_grouping,
-    asset_v1_account_attachments,
+    asset_v1_account_attachments, token_accounts,
     sea_orm_active_enums::{OwnerType, RoyaltyTargetType},
 };
 
@@ -13,6 +13,7 @@ pub enum Relation {
     AssetAuthority,
     AssetCreators,
     AssetGrouping,
+    TokenAccounts,
 }
 
 impl RelationTrait for Relation {
@@ -21,6 +22,10 @@ impl RelationTrait for Relation {
             Self::AssetData => asset::Entity::belongs_to(asset_data::Entity)
                 .from(asset::Column::AssetData)
                 .to(asset_data::Column::Id)
+                .into(),
+            Self::TokenAccounts => asset::Entity::belongs_to(token_accounts::Entity)
+                .from(asset::Column::Id)
+                .to(token_accounts::Column::Mint)
                 .into(),
             Self::AssetV1AccountAttachments => {
                 asset::Entity::has_many(asset_v1_account_attachments::Entity).into()
@@ -59,6 +64,12 @@ impl Related<asset_creators::Entity> for asset::Entity {
 impl Related<asset_grouping::Entity> for asset::Entity {
     fn to() -> RelationDef {
         Relation::AssetGrouping.def()
+    }
+}
+
+impl Related<token_accounts::Entity> for asset::Entity {
+    fn to() -> RelationDef {
+        Relation::TokenAccounts.def()
     }
 }
 
