142764530f2aaec7a31377ab4a7518e821132a43
Nagaprasadvr <nagaprasadvr246@gmail.com>
2024-12-30 17:09:40 +0530

    remove showing token_acc data inside asset data

--- digital_asset_types/src/dao/scopes/asset.rs
+++ digital_asset_types/src/dao/scopes/asset.rs
@@ -15,7 +15,7 @@ use crate::{
 };
 use indexmap::IndexMap;
 use mpl_token_metadata::accounts::{Edition, MasterEdition};
-use sea_orm::sea_query::{Expr, IntoCondition};
+use sea_orm::sea_query::Expr;
 use sea_orm::{entity::*, query::*, ConnectionTrait, DbErr, Order};
 use serde::de::DeserializeOwned;
 use serde_json::Value;
@@ -161,30 +161,14 @@ pub async fn get_assets_by_owner(
     limit: u64,
     options: &Options,
 ) -> Result<Vec<FullAsset>, DbErr> {
-    let mut joins = Vec::new();
-    let mut cond = Condition::all()
+    let cond = Condition::all()
         .add(asset::Column::Owner.eq(owner.clone()))
         .add(asset::Column::Supply.gt(0));
-
-    if options.show_fungible {
-        cond = Condition::all().add(token_accounts::Column::Owner.eq(owner.clone()));
-
-        let rel = extensions::token_accounts::Relation::Asset
-            .def()
-            .rev()
-            .on_condition(|left, right| {
-                Expr::tbl(right, token_accounts::Column::Mint)
-                    .eq(Expr::tbl(left, asset::Column::Id))
-                    .into_condition()
-            });
-
-        joins.push(rel);
-    }
 
     get_assets_by_condition(
         conn,
         cond,
-        joins,
+        vec![],
         sort_by,
         sort_direction,
         pagination,
