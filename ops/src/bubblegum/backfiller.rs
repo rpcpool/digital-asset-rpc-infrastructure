4a4355f3f8c7710a7c88ef75bdf02526db64e789
Fernando <38841537+fernandodeluret@users.noreply.github.com>
2025-11-05 22:30:30 +0000

    `chore` Add final log of counter metrics

--- ops/src/bubblegum/backfiller.rs
+++ ops/src/bubblegum/backfiller.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use clap::Parser;
 use das_bubblegum::{start_backfill, BackfillArgs, BubblegumContext};
 use das_core::{connect_db, PoolArgs, Rpc, SolanaRpcArgs};
+use prometheus::core::Collector;
 
 #[derive(Debug, Parser, Clone)]
 pub struct Args {
@@ -49,5 +50,54 @@ pub async fn run(config: Args) -> Result<()> {
     let solana_rpc = Rpc::from_config(&config.solana);
     let context = BubblegumContext::new(database_pool, solana_rpc);
 
-    start_backfill(context, config.backfill_bubblegum).await
+    start_backfill(context, config.backfill_bubblegum).await?;
+
+    let text = prometheus::TextEncoder::new()
+        .encode_to_string(&das_core::METADATA_JSON_DOWNLOAD_SUCCESS_COUNT.collect())
+        .unwrap_or_else(|e| format!("encode error: {e}"));
+    tracing::info!(
+        "Backfill completed. METADATA_JSON_DOWNLOAD_SUCCESS_COUNT:\n{}",
+        text
+    );
+
+    let text = prometheus::TextEncoder::new()
+        .encode_to_string(&das_core::METADATA_JSON_DOWNLOAD_ERROR_COUNT.collect())
+        .unwrap_or_else(|e| format!("encode error: {e}"));
+    tracing::info!(
+        "Backfill completed. METADATA_JSON_DOWNLOAD_ERROR_COUNT:\n{}",
+        text
+    );
+
+    let text = prometheus::TextEncoder::new()
+        .encode_to_string(
+            &das_bubblegum::metrics::BUBBLEGUM_PROGRAM_TRANSFORMER_ERROR_COUNT.collect(),
+        )
+        .unwrap_or_else(|e| format!("encode error: {e}"));
+    tracing::info!(
+        "Backfill completed. BUBBLEGUM_PROGRAM_TRANSFORMER_ERROR_COUNT:\n{}",
+        text
+    );
+
+    let text = prometheus::TextEncoder::new()
+        .encode_to_string(
+            &program_transformers::metrics::BUBBLEGUM_PROGRAM_TRANSFORMER_SUCCESS_COUNT.collect(),
+        )
+        .unwrap_or_else(|e| format!("encode error: {e}"));
+    tracing::info!(
+        "Backfill completed. BUBBLEGUM_PROGRAM_TRANSFORMER_SUCCESS_COUNT:\n{}",
+        text
+    );
+
+    let text = prometheus::TextEncoder::new()
+        .encode_to_string(
+            &program_transformers::metrics::BUBBLEGUM_DOWNLOAD_METADATA_NOTIFIER_ERROR_COUNT
+                .collect(),
+        )
+        .unwrap_or_else(|e| format!("encode error: {e}"));
+    tracing::info!(
+        "Backfill completed. BUBBLEGUM_DOWNLOAD_METADATA_NOTIFIER_ERROR_COUNT:\n{}",
+        text
+    );
+
+    Ok(())
 }
