3a7c25bbf7043e63279cffde336749ba1581ff56
Fernando <38841537+fernandodeluret@users.noreply.github.com>
2025-11-20 12:41:29 +0000

    `chore` Remove unused struct

--- grpc-ingest/src/config.rs
+++ grpc-ingest/src/config.rs
@@ -510,14 +510,6 @@ impl ConfigBubblegumVerify {
     }
 }
 
-#[derive(Debug, Clone, Deserialize, Default)]
-pub struct DasFumaroleConfig {
-    pub consumer_group_name: String,
-    pub x_subscription_id: String,
-    pub fumarole_endpoint: String,
-    pub fumarole_x_token: Option<String>,
-}
-
 #[derive(Debug, Clone, Deserialize)]
 pub struct ConfigMonitorGaps {
     pub postgres: ConfigPostgres,
